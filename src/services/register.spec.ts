5703fe38489d787a0155e795b47ba7c0c638af62
Gabriel Nogueira <gabrielnogueiraramos@gmail.com>
2024-03-14 19:00:56 -0300

    feat: adiciona teste unitário 'Should hash user password upon registration'

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,32 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterService } from './registerUserService'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2+2).toBe(4)
+describe('Register Service', () => {
+  it('Should hash user password upon registration', async () => {
+    const registerService = new RegisterService({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerService.execute({
+      name: 'john doe',
+      email: 'john@rocketseat.com',
+      password: '1234567',
+    })
+
+    const isPasswordCorrectlyHash = await compare('1234567', user.password_hash)
+    expect(isPasswordCorrectlyHash).toBe(true)
+  })
 })
--- src/services/registerUserService.ts
+++ src/services/registerUserService.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterServiceRequest {
   name: string
@@ -8,9 +9,17 @@ interface RegisterServiceRequest {
   password: string
 }
 
+interface RegisterServiceResponse {
+  user: User
+}
+
 export class RegisterService {
   constructor(private usersRepository: UsersRepository) {}
-  async execute({ name, email, password }: RegisterServiceRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterServiceRequest): Promise<RegisterServiceResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -18,10 +27,11 @@ export class RegisterService {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       email,
       name,
       password_hash,
     })
+    return { user }
   }
 }
