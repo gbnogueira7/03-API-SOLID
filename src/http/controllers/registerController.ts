0d05d65e3cd6e6f4d07ff9461371408729e1e0ea
Gabriel Nogueira <gabrielnogueiraramos@gmail.com>
2024-03-14 16:21:23 -0300

    feat: cria a pasta errors e realiza a devida tratativa ao erro de e-mail

--- src/http/controllers/registerController.ts
+++ src/http/controllers/registerController.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { RegisterService } from '@/services/registerUserService'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-respository'
+import { UserAlreadyExistsError } from '@/services/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -21,7 +22,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (err) {
-    return reply.status(400).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(400).send({ message: err.message })
+    }
+    return reply.status(500)
   }
 
   return reply.status(201).send()
