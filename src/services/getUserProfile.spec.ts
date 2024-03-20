ad21fdfba620489d09a94f2c0c80eecbba9c84a0
Gabriel Nogueira <gabrielnogueiraramos@gmail.com>
2024-03-20 19:17:16 -0300

    feat: adiciona o serviço getUserById, testes e adição no usersRepository

---
+++ src/services/getUserProfile.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { GetUserProfileService } from './get-user-profile'
+import { hash } from 'bcryptjs'
+import { ResourceNotFoundError } from './errors/resource-not-found'
+
+let usersRepository: InMemoryUsersRepository
+let getUserProfileService: GetUserProfileService
+
+describe('find by user id Service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    getUserProfileService = new GetUserProfileService(usersRepository)
+  })
+
+  it('should be able get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'john doe',
+      email: 'john@rocketseat.com',
+      password_hash: await hash('1234567', 6),
+    })
+
+    const { user } = await getUserProfileService.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able get profile with wrong id', async () => {
+    expect(() =>
+      getUserProfileService.execute({
+        userId: 'wrong-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
