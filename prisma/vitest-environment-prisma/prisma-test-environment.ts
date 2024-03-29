b976f2f7e28e4cf2ca75cbe99a98d4ad07a96a2e
Gabriel Nogueira <gabrielnogueiraramos@gmail.com>
2024-03-29 12:43:24 -0300

    test: finalização do environmet de testes e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,37 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
-// sudo npm link dentro da pasta vitest-environment-prisma
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
-
+    const schema = randomUUID()
+    const databaseURL = await generateDatabaseURL(schema)
+    process.env.DATABASE_URL = databaseURL
+    execSync('npx prisma migrate deploy')
     return {
       async teardown() {
-        console.log('Setteardownup')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect
       },
     }
   },
--- src/http/controllers/registe.spec.ts
+++ src/http/controllers/registe.spec.ts
@@ -1,3 +1,7 @@
-import { test } from 'vitest'
+import { expect, test } from 'vitest'
 
-test('ok', () => {})
+test('ok', () => {
+  console.log(process.env.DATABASE_URL)
+
+  expect(1).toBe(1)
+})
